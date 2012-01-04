4b4588aaa8fd9c4a491dcfe0384daa5ba62149e8
Christopher Jones <crjones@gmail.com>
2012-01-04 16:22:02 -0600

    Added validation to ensure Roundtable has a date

--- app/models/roundtable.rb
+++ app/models/roundtable.rb
@@ -2,6 +2,9 @@ class Roundtable < ActiveRecord::Base
   belongs_to :district
   belongs_to :program
   
-  validates_presence_of :num_units, :num_individuals
-  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates_presence_of :happened_on, :num_units, :num_individuals
+  validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  
+  default_scope :order => 'happened_on DESC'
+  
 end
