5420f98d17ce7952693816a485160a4b4d9612fb
Christopher Jones <crjones@gmail.com>
2012-01-04 16:18:28 -0600

    Added initial tests

--- test/unit/district_test.rb
+++ test/unit/district_test.rb
@@ -4,4 +4,8 @@ class DistrictTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "District has a name." do
+    District.create(:name => 'Test District')
+    assert District.name
+  end
 end
--- test/unit/roundtable_test.rb
+++ test/unit/roundtable_test.rb
@@ -4,4 +4,43 @@ class RoundtableTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :roundtables
+  
+  def setup
+    @roundtable = roundtables(:one)
+  end
+  
+  test "Roundtable requires date" do
+    @roundtable.happened_on = nil
+    assert !@roundtable.valid?
+  end
+
+  test "Roundtable requires num_units" do
+    @roundtable.num_units = nil
+    assert !@roundtable.valid?
+  end
+
+  test "Roundtable requires num_individuals" do
+    @roundtable.num_individuals = nil
+    assert !@roundtable.valid?
+  end
+
+  test "Roundtable requires email" do
+    @roundtable.email = nil
+    assert !@roundtable.valid?
+  end
+
+  test "Roundtable email is valid" do
+    @roundtable.email = "foo"
+    assert !@roundtable.valid?
+  end
+  
+  test "Roundtable belongs to a district" do
+    assert_kind_of District, @roundtable.district
+  end
+
+  test "Roundtable belongs to a program" do
+    assert_kind_of Program, @roundtable.program
+  end
+  
 end
