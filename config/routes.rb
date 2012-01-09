ba2ca6d798356866e2443b1f4b02a3770ba634cd
Christopher Jones <crjones@gmail.com>
2012-01-09 10:15:37 -0600

    Organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 RoundtableReport::Application.routes.draw do
   
   resources :programs
-
+  resources :roundtables
+  resources :divisions
+    
   resources :councils do
     resources :divisions
   end
   
-  resources :divisions
-  
   resources :districts do
     resources :roundtables
   end
-  resources :roundtables
+
 
   constraints(Subdomain) do
     match '/' => 'districts#show'
