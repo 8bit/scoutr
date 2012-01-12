c1512fb91ae66d2a095f976825fb9978418779e3
Christopher Jones <crjones@gmail.com>
2012-01-12 15:13:14 -0600

    Validation on Lodges and Chapters

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,6 @@
 class Chapter < ActiveRecord::Base
   belongs_to :district
   belongs_to :lodge
+  
+  validates_presence_of :name, :lodge
 end
--- app/models/lodge.rb
+++ app/models/lodge.rb
@@ -1,4 +1,6 @@
 class Lodge < ActiveRecord::Base
   belongs_to :council
   has_many :chapters
+  
+  validates_presence_of :name
 end
