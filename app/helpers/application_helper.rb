88ed84ac0b92055f852accc60bf55850dd815a93
Christopher Jones <crjones@gmail.com>
2012-01-12 15:09:58 -0600

    Added modal popover for Chapter#new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def render_popup(before,id ,button ,header, body)
+  render :partial => 'shared/popover', 
+         :locals => { :popover_id => id, 
+                      :header => header.html_safe,
+                      :body => body, 
+                      :button => button,
+                      :before => before}
+  end  
 end
