1275ec0694e175564073487bef1db484ca3acb81
Mike Lampkin <mike@lampkin.net>
2012-01-18 20:21:47 -0600

    I be styling.

--- app/helpers/bootstrap_form_builder.rb
+++ app/helpers/bootstrap_form_builder.rb
@@ -12,5 +12,17 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       })
     }
   end
-
+  
+  def date_select(method, options={})
+      t = @template
+      t.content_tag(:div, :class => "clearfix#{' error' unless @object.errors[method].blank?}") {
+        t.concat(t.label_tag method)
+        t.concat(t.content_tag(:div, :class => options[:class].to_s + " input#{' error' unless @object.errors[method].blank?}") {
+          t.concat(super method(options))
+          if @object.errors[method].present?
+            t.concat(t.content_tag(:span, options[:help_text], :class => 'help-inline'))
+          end
+        })
+      }
+    end
 end
